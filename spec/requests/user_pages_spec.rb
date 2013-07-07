e2dd63541447619a53e0240eb6fa1846f5d5e534
Wallsays <derevyankodenis92@gmail.com>
2013-07-07 23:05:31 +0700

    many small changes + test additions

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -30,7 +30,7 @@ describe "User pages" do
         expect { click_button submit }.not_to change(User, :count)
       end
 
-      describe "error messages" do
+      describe "after submission" do
         before { click_button submit }
 
         it { should have_selector('title', text: 'Sign up') }
@@ -57,7 +57,7 @@ describe "User pages" do
 
         it { should have_selector('title', text: user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
-        it { should have_link('Sign out') }
+        # it { should have_link('Sign out') }
       end
     end
   end
